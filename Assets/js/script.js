6d4bbdd8896240451bd95c4da1ead2827edab28f
Princess M <princessmosse@gmail.com>
2021-02-10 21:29:07 -0500

    adding variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,3 +12,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Variables
+var numbers = ["0", "1" "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "#", "$", "%", "^", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";","<", "?", "[", "/", "]", "-", "{", "|", "}", "~"];
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "s", "r", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "S", "R", "T", "U", "V", "W", "X", "Y", "Z"];
