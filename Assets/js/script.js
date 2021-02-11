2f6c93f1000a4ed4b2093a9af6c8135134c90760
Princess M <princessmosse@gmail.com>
2021-02-10 23:39:02 -0500

    update all files

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,20 +1,64 @@
-// Assignment Code
+// Assignment Code-given
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+//Global variables
+var specialChar = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+// Password Function
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  if (password) {
+    passwordText.value = password;
+  }
+};
 
-  passwordText.value = password;
+//Functions
+function generatePassword() {
+  var passwordString = '';//password string
+  var arrayHolderPassword = []
+
+  //length of password
+  var numberOfCharacters = window.prompt('How many characters would you like your password to contain? Choose between 8 and 128.');
+  if (numberOfCharacters < 8 || numberOfCharacters > 128 || isNaN(numberOfCharacters)) {
+    alert('This needs a value between 8 and 128! Start Over.')
+    return; 
+  }
+
+  var chooseSpecialChar = window.confirm('Do you want special characters to be included?');
+  var chooseupperCase = window.confirm('Do you want upper case characters to be included?');
+  var chooselowerCase = window.confirm('Do you want lower case characters to be included?');
+  var choosenumbersChar = window.confirm('Do you want numeric characters to be included?');
+
+  if (chooseSpecialChar) {
+    Array.prototype.push.apply(arrayHolderPassword, specialChar);
+  }
+
+  if (chooseNumbers) {
+    Array.prototype.push.apply(arrayHolderPassword, numbers);
+  }
+
+  if (chooseLowerCase) {
+    Array.prototype.push.apply(arrayHolderPassword, lowerCase);
+  }
+  if (chooseUpperCase) {
+    Array.prototype.push.apply(arrayHolderPassword, upperCase);
+  }
+
+  //for loop to loop through the new array arrayHolderPassword with user input length stored in numberOfCharacters
+  for (var i = 0; i < numberOfCharacters; i++) {
+    var newPasswordIndex = Math.floor(Math.random() * arrayHolderPassword.length);
+    passwordString = passwordString + arrayHolderPassword[newPasswordIndex];
+  }
+  return (passwordString);
+} 
+
+// Add event listener to generate button-given
+generateBtn.addEventListener("click", writePassword); {
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//Variables
-var numbers = ["0", "1" "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "#", "$", "%", "^", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";","<", "?", "[", "/", "]", "-", "{", "|", "}", "~"];
-var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "s", "r", "t", "u", "v", "w", "x", "y", "z"];
-var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "S", "R", "T", "U", "V", "W", "X", "Y", "Z"];
--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # JavaScript-Password-Generator
 
+![screenshot of webpage](assets/images/_Users_evonna_CodingBootcamp_homework_JavaScript-Password-Generator_index.html.png)
+
+
+
+
+
+A client wants a clean and polished, responsive user interface that will help generate random passowrds to provide greater securtiy to their employees. To meet the certain criteria, the passwords must include special characters, numeric and/or upper and lower case characters between 8-128 characters. Once the employee chooses the options they want the password will be generated for the employees discretion.
+
+
 
 
 
